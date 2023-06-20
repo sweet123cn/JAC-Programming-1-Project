b87232127b98d994bbd592d49b8411c4ab0e5422
DavidTang <sweet123cn@outlook.com>
2023-06-20 11:39:43 -0400

    add Question06.java

--- src/Question06.java
+++ src/Question06.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
 
 public class Question06 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner kb = new Scanner(System.in);
+
+        System.out.print("Enter the array element: ");
+        String line = kb.nextLine();
+
+        // Split the input line with ',' seperated into individual integers
+        String[] nums = line.split(",");
+
+        // Convert the strings to integers
+        int[] myarr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            myarr[i] = Integer.parseInt(nums[i]);
+        }
+		System.out.println("the array you input:");
+		printIntArray (myarr);
+
+		kb.close();
 	}
 
 }
