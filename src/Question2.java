82d0160988f17db6f2e39d54a3c0781f65ab6073
xingr121 <137085577+xingr121@users.noreply.github.com>
2023-06-20 19:31:01 -0400

    Update Question2.java

--- src/Question2.java
+++ src/Question2.java
@@ -5,13 +5,19 @@ public class Question2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner kb = new Scanner(System.in);
+
+//		Prompt the user to enter a number as joker		
 		System.out.println("Joker: ");
 		int joker = kb.nextInt();
+
+//		Prompt the user to enter a value how many number he/she wants to put and input numbers 		
 		System.out.println("Number: ");
 		int n = kb.nextInt();
 		System.out.println("Enter number: ");
+
+//		Follow the rule to print the numbers		
 		int num ;
-		// according the explanation of the question text:
+// according the explanation of the question text:
 		// for(int i = 1; i <= n; i++) {
 		// 	int temp = kb.nextInt();
 		// 	if (temp >= joker) {
@@ -21,7 +27,8 @@ public class Question2 {
 		// 	}
 		// 	System.out.print(num + " ");
 		// }
-		// according the output examples:
+		
+// according the output examples:
 		for(int i = 1; i <= n; i++) {
 			int temp = kb.nextInt();
 		        num = temp % joker;
