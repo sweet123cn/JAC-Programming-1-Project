81b745bf23ba35aa26cdffd072eb85021172f359
DavidTang <sweet123cn@outlook.com>
2023-06-20 11:32:33 -0400

    finish Question04

--- src/Question04.java
+++ src/Question04.java
@@ -1,9 +1,82 @@
+import java.util.Scanner;
+import java.util.Random;
 
 public class Question04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
+
+		System.out.println("please input the size of array?");
+		int size = kb.nextInt();
+
+		while (size <= 0)
+		{
+			System.out.println("Array size error, please input size of array:");
+			size = kb.nextInt();
+		}
+ 
+		int radonRange = 100; //random number range:0-99
+
+		int i, temp;
+		
+		int[] myarr1 = randomIntArray(size, radonRange); //generate a random integer array
+
+		for ( i = 0 ; i < myarr1.length; i++) //sort
+		{
+			for (int j = i ; j < myarr1.length; j++)
+			{
+				if ( myarr1[i] < myarr1[j])
+				{
+					temp = myarr1[i];
+					myarr1[i] = myarr1[j];
+					myarr1[j] = temp;
+				}
+			} 
+		}
+
+		System.out.println("=======the array sorted=========");
+
+		printIntArray(myarr1); //print the sorted array
+
+		kb.close();
 
 	}
 
+	public static int[] randomIntArray(int size,int range)
+	{
+		System.out.println("====generate random array======="); 
+
+		Random rand = new Random();
+
+		int[] myarr =new int[size];
+
+		for ( int i = 0; i < size; i++)
+		{
+			myarr[i] = rand.nextInt(range);
+		}
+
+		printIntArray( myarr );
+
+		return myarr;
+
+	}
+
+	public static void printIntArray( int[] arr )
+	{
+		for ( int i = 0; i < arr.length; i++)
+		{
+			System.out.printf("%2d ",arr[i]);
+
+			if ( (i+1) %10 == 0)
+			{
+				System.out.println();
+			}
+
+		}
+
+		System.out.println();
+	}
+
 }
+
--- src/Question05.java
+++ src/Question05.java
@@ -1,9 +1,88 @@
+import java.util.Scanner;
 
 public class Question05 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner kb = new Scanner(System.in);
 
+        System.out.print("Enter the array element: ");
+        String line = kb.nextLine();
+
+        // Split the input line with ',' seperated into individual integers
+        String[] nums = line.split(",");
+
+        // Convert the strings to integers
+        int[] myarr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            myarr[i] = Integer.parseInt(nums[i]);
+        }
+		System.out.println("the array you input:");
+		printIntArray (myarr);
+
+		int[] duplicateArray = findDuplicateIntArray(myarr); //count duplicate numbers
+
+		System.out.println("array element duplicate times:");
+		printIntArray (duplicateArray);
+
+		boolean flag = false;
+
+		for ( int j = 0 ;j < myarr.length; j ++)
+		{
+			if ( duplicateArray[j] > myarr.length/2)
+			{
+				System.out.println("the majority element is :" + myarr[j]);
+				flag = true;
+				break;
+			}
+
+		}
+
+		if (!flag)
+		{
+			System.out.println("there is no majority element which appears more than " + (myarr.length/2) + " times");
+		}
+
+		kb.close();
+	}
+
+	public static void printIntArray( int[] arr )
+	{
+		for ( int i = 0; i < arr.length; i++)
+		{
+			System.out.printf("%2d ",arr[i]);
+
+			if ( (i+1) %10 == 0)
+			{
+				System.out.println();
+			}
+
+		}
+
+		System.out.println();
+	}
+
+	public static int[] findDuplicateIntArray(int[] myArray)
+	{
+		int[] duplicate = new int[myArray.length];
+
+		int count = 0;
+
+		for (int i = 0; i < myArray.length; i++ )
+		{
+			for (int j : myArray)
+			{
+				if (myArray[i] == j)
+				{
+					count++;
+				}
+			}
+
+			duplicate[i] = count;
+			count = 0;
+		}
+
+		return duplicate;
 	}
 
 }
