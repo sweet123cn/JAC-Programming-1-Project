bee374e178a69f86b217f70d42bf537aa1e091d5
xingr121 <137085577+xingr121@users.noreply.github.com>
2023-06-20 19:27:42 -0400

    Update Question3.java

--- src/Question3.java
+++ src/Question3.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+
 import java.util.Scanner;
 
 public class Question3 {
@@ -8,33 +8,35 @@ public class Question3 {
 
 		int i, j;
         Scanner kb = new Scanner(System.in);
-		
+
+//		Prompt the user to enter two numbers as the dimension of a array; 		
 		System.out.println("Enter two numbers: ");
 		int a = kb.nextInt();
 		int b = kb.nextInt();
-		
 		int[][] myarr = new int[a][b];
 		
-		
-	    Random rand = new Random();
-	    
+//	    Create the array  		
 	    for (i = 0; i < a; i++) {
 	    	for(j = 0; j < b; j++) {
-	    		
-	    	// Dont use any libraty, how about Math.random
-	    		myarr[i][j] = rand.nextInt(100);	
+	    	myarr[i][j] = (int)(Math.random() * 100 + 1);	
 	    	}
 	    }
+		
+//	    print the array		
 	    System.out.println("Original Array: ");
+		
 	    for (i = 0; i < myarr.length; i++) {
    	        for(j = 0; j < myarr[i].length; j++) {
     		System.out.printf("%4d", myarr[i][j]);
     	}
     	System.out.println();
 	    }
+
+//	    Prompt the user to enter a number  to disappear		
     	System.out.println("which number you want to disappear:  ");
     	int num = kb.nextInt();
-    	
+
+//    	Search the number in the array and replaced  by 0		
     	 for (i = 0; i < myarr.length; i++) {
     	        for(j = 0; j < myarr[i].length; j++) {
      		if (myarr[i][j] == num) {
@@ -42,6 +44,8 @@ public class Question3 {
      		}        	
      	}
     	 }
+
+//    	 Print the new array		
         System.out.println("New Array: ");
         for (i = 0; i < myarr.length; i++) {
     	   	 for(j = 0; j < myarr[i].length; j++) {
